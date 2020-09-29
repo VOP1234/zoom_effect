d9b7d87ccae4d4a8529de16782bd56a8bc9050d5
Vitor <vop1234@hotmail.com>
2020-09-28 23:53:59 -0300

    Finishing

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const container = document.getElementById('container')
+
+const image = document.querySelector('img')
+
+container.addEventListener('mouseover', (e) => {
+  const x = e.clientX - e.target.offsetLeft
+  const y = e.clientY - e.target.offsetTop
+
+  image.style.transformOrigin = `${x}px ${y}px`
+
+  image.style.transform = 'scale(2)'
+
+})
+
+container.addEventListener('mouseleave', () => {
+  image.style.transformOrigin = "center center"
+  image.style.transform = 'scale(1)'
+})
